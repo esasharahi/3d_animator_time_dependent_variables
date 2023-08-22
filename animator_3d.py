ead2017f8071e400130ff7d8844c45fd97f4519b
esasharahi <esasharahi@gmail.com>
2023-08-22 16:24:07 +0330

    Create animator_3d.py

---
+++ animator_3d.py
@@ -0,0 +1,27 @@
+# esasharahi@gmail.com
+
+import pandas as pd
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('data.csv')
+culs_names = list(df.columns)
+
+t = df[culs_names[0]]
+x = df[culs_names[1]]
+y = df[culs_names[2]]
+z = df[culs_names[3]]
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+
+def update_plot(i):
+    ax.cla()
+    ax.plot(x[:i], y[:i], z[:i])
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    ax.set_title(f'time: {t[i]}')
+
+ani = animation.FuncAnimation(fig, update_plot, frames=len(t), interval=150)
+plt.show()
